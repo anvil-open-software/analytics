e210c3bef2449f1e1a8d4e0deea3521f2f23eb54
mitchemr <michael.mitchell@dematic.com>
2015-01-29 16:32:57 -0800

    DLABS-33_2 - Event processor.

---
+++ analytics/ingestion/src/main/java/com/dematic/dlabs/analytics/ingestion/EventProducer.java
@@ -0,0 +1,35 @@
+package com.dematic.dlabs.analytics.ingestion;
+
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+
+import java.io.Serializable;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * Class will pull events from a Kinesis stream and process the events.
+ *
+ */
+@SuppressWarnings("UnusedDeclaration")
+public final class EventProducer implements Serializable {
+    public void process(final JavaReceiverInputDStream<byte[]> inputStream) {
+        // transform the byte[] to a string
+        final JavaDStream<String> eventMap =
+                inputStream.map(
+                        event -> new String(event, Charset.defaultCharset())
+                );
+        // for each distributed data set, send to an output stream, for now we just log
+        eventMap.foreachRDD(
+                // rdd = distributed data set
+                rdd -> {
+                    if (rdd.count() > 0) {
+                        final List<String> events = rdd.collect();
+                        IntStream.range(0, events.size()).forEach(System.out::print);
+                    }
+                    return null;
+                }
+        );
+    }
+}
