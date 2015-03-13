1d48911125f3da7e7702da0ef7ae72c00ca0bff8
Silveira, Rodrigo <Rodrigo.Silveira@dematic.com>
2015-03-12 20:53:39 -0700

    DLABS-93 - Added resource/token call.

--- nextgen/ngclient/src/main/webapp/app/js/services/authentication_service.js
+++ nextgen/ngclient/src/main/webapp/app/js/services/authentication_service.js
@@ -7,7 +7,16 @@ angular.module("Authentication")
   // these routes map to stubbed API endpoints in config/server.js
   return {
     login: function(credentials) {
-      return $http.post('/login', credentials);
+        var request = {
+            method: 'GET',
+            url: 'resources/token',
+            headers: {
+                "Accept": "application/json",
+                "Authorization": "DLabsU " + "Dematic" + ":" + credentials.username + ":" + credentials.password
+            }
+        };
+
+      return $http(request);
     },
     logout: function() {
       return $http.post('/logout');
