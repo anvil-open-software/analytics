af164d61fbc5431dff52fdd12fc619721cfc74a4
Silveira, Rodrigo <Rodrigo.Silveira@dematic.com>
2015-03-16 22:08:07 -0700

    DLABS-93 The integration tests

---
+++ nextgen/ngclient/src/test/java/com/dematic/labs/ngclient/NgClientIT.java
@@ -0,0 +1,12 @@
+package com.dematic.labs.ngclient;
+
+import javax.annotation.sql.DataSourceDefinition;
+import javax.ejb.Stateless;
+
+@DataSourceDefinition(name = "java:global/NextGen/NextGenDataSource",
+                      className = "com.mysql.jdbc.jdbc2.optional.MysqlDataSource",
+                      url = "jdbc:mysql://127.0.0.1:3306/nextgen",
+                      user = "root")
+@Stateless
+public class NgClientIT {
+}
