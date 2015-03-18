3a75904cd0d64dc3a79765f83424ecbb93908f04
Silveira, Rodrigo <Rodrigo.Silveira@dematic.com>
2015-03-18 13:52:01 -0700

    DLABS-93 Moved the webdriver.chrome.driver to the systemPropertyVariables section in the nextgen.pom maven-surefire-plugin declaration

--- nextgen/integration/src/test/java/com/dematic/labs/ngclient/NgClientIT.java
+++ nextgen/integration/src/test/java/com/dematic/labs/ngclient/NgClientIT.java
@@ -27,13 +27,12 @@ public class NgClientIT {
 
     @BeforeClass
     public static void before() throws MalformedURLException {
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         driver = new ChromeDriver();
     }
 
     @Test
     public void test1GetLandingPage ()  throws MalformedURLException {
-        // Little change to force a build ..
+        // Little change to force a build ...
         driver.get("http://127.0.0.1:8080/ngclient/");
         System.out.print("Title: " + driver.getTitle());
         assertTrue(driver.getTitle().startsWith("ngclient"));
