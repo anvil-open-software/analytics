7f2ba5176648f94debb018f992d1b7d5fcc68418
dematic-mark-eggleston <mark.eggleston@dematic.com>
2015-03-24 08:14:47 -0700

    DLABS-65 Add Sonar to pom and correct package cycle involving SortDirection

--- nextgen/admin/src/test/java/com/dematic/labs/rest/helpers/OrderByQueryParameterConverterTest.java
+++ nextgen/admin/src/test/java/com/dematic/labs/rest/helpers/OrderByQueryParameterConverterTest.java
@@ -1,7 +1,7 @@
 package com.dematic.labs.rest.helpers;
 
 import com.dematic.labs.persistence.query.QueryParameters;
-import com.dematic.labs.persistence.entities.SortDirection;
+import com.dematic.labs.persistence.query.SortDirection;
 import org.hamcrest.collection.IsIterableContainingInOrder;
 import org.junit.Rule;
 import org.junit.Test;
--- nextgen/persistence/src/main/java/com/dematic/labs/persistence/query/QueryParametersHelper.java
+++ nextgen/persistence/src/main/java/com/dematic/labs/persistence/query/QueryParametersHelper.java
@@ -1,6 +1,5 @@
 package com.dematic.labs.persistence.query;
 
-import com.dematic.labs.persistence.entities.SortDirection;
 import com.mysema.query.types.Order;
 import com.mysema.query.types.Path;
 import com.mysema.query.types.path.EntityPathBase;
