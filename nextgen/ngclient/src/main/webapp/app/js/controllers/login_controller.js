a965cf3348e5d126d1a963a021b7548e7d7f5c19
Silveira, Rodrigo <Rodrigo.Silveira@dematic.com>
2015-03-24 19:39:51 -0500

    DLABS-128 - Added code to support provisioning the service that keeps the user credentials available.

--- nextgen/ngclient/src/main/webapp/app/js/controllers/login_controller.js
+++ nextgen/ngclient/src/main/webapp/app/js/controllers/login_controller.js
@@ -3,10 +3,10 @@
 */
 angular.module('Authentication')
 
-.controller('LoginController', ['$scope', '$location', 'AuthenticationService', 'AuthenticatedUser',
-    function($scope, $location, AuthenticationService, AuthenticatedUser) {
+.controller('LoginController', ['$scope', '$location', 'AuthenticationService', 'AuthenticatedUser', 'SecurityToken',
+    function($scope, $location, AuthenticationService, AuthenticatedUser, SecurityToken) {
         $scope.title = "Landing";
-        $scope.credentials = { username: "", password: "" };
+        $scope.credentials = { username: "superuser", password: "abcd1234" };
 
         var onLoginSuccess = function() {
             $location.path('/home');
@@ -15,6 +15,7 @@ angular.module('Authentication')
         $scope.login = function() {
             AuthenticationService.login($scope.credentials).success(function(data) {
                 AuthenticatedUser.setAuthenticatedUser(data);
+                SecurityToken.set(data);
                 onLoginSuccess();
             });
         };
