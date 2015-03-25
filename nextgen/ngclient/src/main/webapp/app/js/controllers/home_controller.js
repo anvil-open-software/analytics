08c3361bf73cd9199baf4ed0a81712d472571f8c
Silveira, Rodrigo <Rodrigo.Silveira@dematic.com>
2015-03-24 19:38:18 -0500

    DLABS-128 Hooked up the tenants scope variable to the TenantResourceService

--- nextgen/ngclient/src/main/webapp/app/js/controllers/home_controller.js
+++ nextgen/ngclient/src/main/webapp/app/js/controllers/home_controller.js
@@ -4,19 +4,25 @@
 
 angular.module('Authentication')
 
-.controller('HomeController', ['$scope', '$location', 'AuthenticationService', 'AuthenticatedUser',
-    function($scope, $location, AuthenticationService, AuthenticatedUser) {
+.controller('HomeController', ['$scope', '$location', 'AuthenticatedUser', 'TenantResourceSrvc',
+    function($scope, $location, AuthenticatedUser, TenantResourceSrvc) {
         $scope.title = "Home";
         $scope.message = "Mouse Over these images to see a directive at work";
         $scope.authenticatedUser = AuthenticatedUser.getAuthenticatedUser();
-        $scope.tenants = [{name: 'Dematic'}, {name: 'Safeway'}, {name: 'Wallmart'}, {name: 'UPS'}];
+        //$scope.tenants = [{name: 'Dematic'}, {name: 'Safeway'}, {name: 'Wallmart'}, {name: 'UPS'}];
+        $scope.tenants = [];
+        $scope.showDetails = false;
 
         var onLogoutSuccess = function (response) {
             $location.path('/login');
         };
 
+        $scope.showTenants = function(event) {
+            $scope.showDetails = !$scope.showDetails;
+            $scope.tenants = $scope.showDetails ? TenantResourceSrvc.query() : [];
+        };
+
         $scope.logout = function () {
-            //AuthenticationService.logout().success(onLogoutSuccess);
             onLogoutSuccess();
         };
     }
