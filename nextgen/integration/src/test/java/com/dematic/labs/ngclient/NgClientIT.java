4d958def25e55955069f8ccf5264e59399412e1c
dematic-mark-eggleston <mark.eggleston@dematic.com>
2015-03-26 08:07:21 -0700

    DLABS-128 code review

--- nextgen/integration/src/test/java/com/dematic/labs/ngclient/NgClientIT.java
+++ nextgen/integration/src/test/java/com/dematic/labs/ngclient/NgClientIT.java
@@ -1,42 +1,41 @@
 package com.dematic.labs.ngclient;
 
+import com.dematic.labs.picketlink.SecurityInitializer;
+import com.dematic.labs.rest.SecuredEndpointHelper;
 import org.junit.*;
 import org.junit.runners.MethodSorters;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.MalformedURLException;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class NgClientIT {
 
     private static WebDriver driver;
-    private static String homePage = "http://127.0.0.1:8080/ngclient/";
+    private static String homePage = SecuredEndpointHelper.BASE_URL;
 
     public NgClientIT() { }
 
     @BeforeClass
-    public static void beforeClass() throws MalformedURLException {
+    public static void beforeClass() {
         driver = new ChromeDriver();
     }
 
     @Test
-    public void test0000GetLandingPage ()  throws MalformedURLException {
+    public void test0000GetLandingPage ()  {
         // Little change to force a build ...
         driver.get(homePage);
         String title = driver.getTitle();
-        assertEquals(title.compareTo("ngclient"), 0);
+        assertEquals(title.compareTo(SecuredEndpointHelper.CONTEXT_ROOT), 0);
     }
 
     @Test
-    public void test0100Login ()  throws MalformedURLException {
+    public void test0100Login ()  {
         WebElement username;
         WebElement password;
         WebElement login;
@@ -55,9 +54,9 @@ public class NgClientIT {
         login    = (new WebDriverWait(driver, 2)).until(ExpectedConditions.presenceOfElementLocated(By.id("log-in")));
 
         username.clear();
-        username.sendKeys("superuser");
+        username.sendKeys(SecurityInitializer.INSTANCE_ADMIN_USERNAME);
         password.clear();
-        password.sendKeys("abcd1234");
+        password.sendKeys(SecurityInitializer.INSTANCE_ADMIN_PASSWORD);
         login.submit();
 
         wait = new WebDriverWait(driver, 40);
@@ -70,7 +69,7 @@ public class NgClientIT {
     }
 
     @Test
-    public void test0200AuthenticateValidRequest ()  throws MalformedURLException {
+    public void test0200AuthenticateValidRequest ()  {
         WebElement tenantClick;
         WebElement tenants;
         String tenants_class;
