d912993e379ac1795101833caed181b02bc5cf28
Silveira, Rodrigo <Rodrigo.Silveira@dematic.com>
2015-03-30 09:12:12 -0700

    DL-55 Now that the basic unit tests are passing, integrated into application.

--- nextgen/ngclient/src/main/webapp/app/js/router.js
+++ nextgen/ngclient/src/main/webapp/app/js/router.js
@@ -3,12 +3,14 @@ angular.module("app").config(function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode({enabled:true});
 
     $routeProvider.when('/', {
-        templateUrl: 'login.html',
+        //templateUrl: 'login.html',
+        templateUrl: 'dlSigninPrompt.html',
         controller: 'LoginController'
     });
 
   $routeProvider.when('/login', {
-    templateUrl: 'login.html',
+    //templateUrl: 'login.html',
+    templateUrl: 'dlSigninPrompt.html',
     controller: 'LoginController'
   });
 
