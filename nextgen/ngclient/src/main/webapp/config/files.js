d0823b0ad41c5a70fef61d4fafba15a0c58420e1
Silveira, Rodrigo <Rodrigo.Silveira@dematic.com>
2015-04-02 15:41:39 -0700

    DLABS-55 Changes to load the HMAC-SHA1 library locally instead of from its own website.

--- nextgen/ngclient/src/main/webapp/config/files.js
+++ nextgen/ngclient/src/main/webapp/config/files.js
@@ -10,6 +10,11 @@ module.exports = function(lineman) {
   return {
     js: {
       vendor: [
+        "vendor/js/hmac-md5.js",
+        "vendor/js/hmac-sha1.js",
+        "vendor/js/hmac-core-min.js",
+        "vendor/js/hmac-enc-utf16-min.js",
+        "vendor/js/hmac-enc-base64-min.js",
         "vendor/js/jQuery.js",
         "vendor/js/bootstrap.js",
         "vendor/js/angular.js",
