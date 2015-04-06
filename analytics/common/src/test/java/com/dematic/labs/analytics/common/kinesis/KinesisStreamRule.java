9cb86cd60f65197eadbf717f1414fcc554f52197
mitchemr <michael.mitchell@dematic.com>
2015-04-06 13:54:22 -0700

    DLABS-135, Added utility class and cleanup.

--- analytics/common/src/test/java/com/dematic/labs/analytics/common/kinesis/KinesisStreamRule.java
+++ analytics/common/src/test/java/com/dematic/labs/analytics/common/kinesis/KinesisStreamRule.java
@@ -2,26 +2,27 @@ package com.dematic.labs.analytics.common.kinesis;
 
 import com.amazonaws.services.kinesis.AmazonKinesisClient;
 import com.amazonaws.services.kinesis.connectors.KinesisConnectorConfiguration;
+import com.dematic.labs.analytics.common.AWSConnections;
 import org.junit.rules.ExternalResource;
 
 
-public class KinesisStreamRule extends ExternalResource {
+public final class KinesisStreamRule extends ExternalResource {
     public KinesisConnectorConfiguration getKinesisConnectorConfiguration() {
-        return Bootstrap.getKinesisConnectorConfiguration(Bootstrap.getAWSCredentialsProvider());
+        return AWSConnections.getKinesisConnectorConfiguration(AWSConnections.getAWSCredentialsProvider());
     }
 
     public AmazonKinesisClient getAmazonKinesisClient() {
-        return Bootstrap.getClient(getKinesisConnectorConfiguration());
+        return AWSConnections.getAmazonKinesisClient(getKinesisConnectorConfiguration());
     }
 
     @Override
     protected void before() throws Throwable {
         // 1) create the kinesis streams
-        Bootstrap.createStreams(getAmazonKinesisClient(), getKinesisConnectorConfiguration());
+        AWSConnections.createKinesisStreams(getAmazonKinesisClient(), getKinesisConnectorConfiguration());
     }
 
     @Override
     protected void after() {
-        Bootstrap.destroyStream(getAmazonKinesisClient(), getKinesisConnectorConfiguration());
+        AWSConnections.destroyKinesisStream(getAmazonKinesisClient(), getKinesisConnectorConfiguration());
     }
 }
