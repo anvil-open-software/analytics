3c8db8f60b46bbe38da0cb5f8c5b65ddc640636a
mitchemr <michael.mitchell@dematic.com>
2015-04-13 15:33:43 -0700

    DLABS-135, Test cleanup.

--- analytics/common/src/test/java/com/dematic/labs/analytics/common/kinesis/KinesisStreamRule.java
+++ analytics/common/src/test/java/com/dematic/labs/analytics/common/kinesis/KinesisStreamRule.java
@@ -2,19 +2,19 @@ package com.dematic.labs.analytics.common.kinesis;
 
 import com.amazonaws.services.kinesis.AmazonKinesisClient;
 import com.amazonaws.services.kinesis.connectors.KinesisConnectorConfiguration;
+import com.amazonaws.services.kinesis.model.PutRecordRequest;
 import com.dematic.labs.analytics.common.AWSConnections;
+import com.dematic.labs.analytics.common.Event;
+import com.dematic.labs.analytics.common.kinesis.consumer.EventToByteArrayTransformer;
+import org.joda.time.DateTime;
 import org.junit.rules.ExternalResource;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.UUID;
 
 
 public final class KinesisStreamRule extends ExternalResource {
-    public KinesisConnectorConfiguration getKinesisConnectorConfiguration() {
-        return AWSConnections.getKinesisConnectorConfiguration(AWSConnections.getAWSCredentialsProvider());
-    }
-
-    public AmazonKinesisClient getAmazonKinesisClient() {
-        return AWSConnections.getAmazonKinesisClient(getKinesisConnectorConfiguration());
-    }
-
     @Override
     protected void before() throws Throwable {
         // 1) create the kinesis streams
@@ -24,5 +24,26 @@ public final class KinesisStreamRule extends ExternalResource {
     @Override
     protected void after() {
         AWSConnections.destroyKinesisStream(getAmazonKinesisClient(), getKinesisConnectorConfiguration());
+    }
+
+    public KinesisConnectorConfiguration getKinesisConnectorConfiguration() {
+        return AWSConnections.getKinesisConnectorConfiguration(AWSConnections.getAWSCredentialsProvider());
+    }
+
+    public AmazonKinesisClient getAmazonKinesisClient() {
+        return AWSConnections.getAmazonKinesisClient(getKinesisConnectorConfiguration());
+    }
+
+    public void generateEventsAndPush(final int count) throws IOException {
+        // push events to a Kinesis stream
+        for (int i = 1; i <= count; i++) {
+            final PutRecordRequest putRecordRequest = new PutRecordRequest();
+            putRecordRequest.setStreamName(getKinesisConnectorConfiguration().KINESIS_INPUT_STREAM);
+            final Event event = new Event(UUID.randomUUID(), i, i, DateTime.now(), i);
+            putRecordRequest.setData(ByteBuffer.wrap(new EventToByteArrayTransformer().fromClass(event)));
+            // partition key = which shard to send the request,
+            putRecordRequest.setPartitionKey("1");
+            getAmazonKinesisClient().putRecord(putRecordRequest);
+        }
     }
 }
--- analytics/integration/src/test/java/com/dematic/labs/analytics/integration/EventProcessingWorkflowIT.java
+++ analytics/integration/src/test/java/com/dematic/labs/analytics/integration/EventProcessingWorkflowIT.java
@@ -3,21 +3,23 @@ package com.dematic.labs.analytics.integration;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.kinesis.AmazonKinesisClient;
 import com.amazonaws.services.kinesis.connectors.KinesisConnectorConfiguration;
-import com.amazonaws.services.kinesis.model.*;
-import com.dematic.labs.analytics.common.Event;
+import com.amazonaws.services.kinesis.model.DescribeStreamResult;
+import com.amazonaws.services.kinesis.model.GetRecordsRequest;
+import com.amazonaws.services.kinesis.model.GetRecordsResult;
+import com.amazonaws.services.kinesis.model.GetShardIteratorRequest;
+import com.amazonaws.services.kinesis.model.GetShardIteratorResult;
+import com.amazonaws.services.kinesis.model.Record;
+import com.amazonaws.services.kinesis.model.Shard;
+import com.amazonaws.services.kinesis.model.ShardIteratorType;
 import com.dematic.labs.analytics.common.kinesis.KinesisStreamRule;
-import com.dematic.labs.analytics.common.kinesis.consumer.EventToByteArrayTransformer;
 import com.dematic.labs.analytics.ingestion.sparks.DriverUtils;
 import com.jayway.awaitility.Awaitility;
-import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import static com.dematic.labs.analytics.common.AWSConnections.getAWSCredentialsProvider;
@@ -40,16 +42,7 @@ public final class EventProcessingWorkflowIT {
     }
 
     private void produceEventsToKinesis() throws IOException {
-        // push events to a Kinesis stream
-        for (int i = 1; i <= EVENT_COUNT; i++) {
-            final PutRecordRequest putRecordRequest = new PutRecordRequest();
-            putRecordRequest.setStreamName(kinesisStreamRule.getKinesisConnectorConfiguration().KINESIS_INPUT_STREAM);
-            final Event event = new Event(UUID.randomUUID(), i, i, DateTime.now(), i);
-            putRecordRequest.setData(ByteBuffer.wrap(new EventToByteArrayTransformer().fromClass(event)));
-            // partition key = which shard to send the request,
-            putRecordRequest.setPartitionKey("1");
-            kinesisStreamRule.getAmazonKinesisClient().putRecord(putRecordRequest);
-        }
+       kinesisStreamRule.generateEventsAndPush(EVENT_COUNT);
     }
 
     // ensure all events are pulled from the kinesis stream, sparks is consuming the events
