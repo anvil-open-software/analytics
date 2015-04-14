04d8300fb25e4a53adfeb689219dd87bc6765367
mitchemr <michael.mitchell@dematic.com>
2015-04-14 14:18:41 -0700

    DLABS-135, Cleanup.

--- nextgen/producers/src/main/java/com/dematic/labs/producers/kinesis/ClientProducer.java
+++ nextgen/producers/src/main/java/com/dematic/labs/producers/kinesis/ClientProducer.java
@@ -1,25 +1,14 @@
 package com.dematic.labs.producers.kinesis;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.services.kinesis.AmazonKinesisClient;
-import com.amazonaws.services.kinesis.connectors.KinesisConnectorConfiguration;
 import com.dematic.labs.analytics.common.AWSConnections;
 
 import javax.enterprise.inject.Produces;
 
 public final class ClientProducer {
+    //todo: will come back to this when nextgen starts starts to push events
     @Produces
     public static AWSCredentialsProvider getAWSCredentialsProvider() {
         return AWSConnections.getAWSCredentialsProvider();
-    }
-
-    @Produces
-    public static KinesisConnectorConfiguration getKinesisConnectorConfiguration(@SuppressWarnings("CdiInjectionPointsInspection") final AWSCredentialsProvider credentialsProvider) {
-        return AWSConnections.getKinesisConnectorConfiguration(credentialsProvider);
-    }
-
-    @Produces
-    public static AmazonKinesisClient getClient(final KinesisConnectorConfiguration kinesisConnectorConfiguration) {
-        return AWSConnections.getAmazonKinesisClient(kinesisConnectorConfiguration);
     }
 }
