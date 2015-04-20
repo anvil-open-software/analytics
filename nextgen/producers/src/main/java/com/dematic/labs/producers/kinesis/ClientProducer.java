249c4fdb131935eb633f742aaf59c8e9fbe9482a
mitchemr <michael.mitchell@dematic.com>
2015-04-20 15:43:13 -0700

    DLABS-163 updating nextgen to use commons/toolkit.

--- nextgen/producers/src/main/java/com/dematic/labs/producers/kinesis/ClientProducer.java
+++ nextgen/producers/src/main/java/com/dematic/labs/producers/kinesis/ClientProducer.java
@@ -1,7 +1,7 @@
 package com.dematic.labs.producers.kinesis;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.dematic.labs.analytics.common.AWSConnections;
+import com.dematic.labs.toolkit.aws.Connections;
 
 import javax.enterprise.inject.Produces;
 
@@ -9,6 +9,6 @@ public final class ClientProducer {
     //todo: will come back to this when nextgen starts starts to push events
     @Produces
     public static AWSCredentialsProvider getAWSCredentialsProvider() {
-        return AWSConnections.getAWSCredentialsProvider();
+        return Connections.getAWSCredentialsProvider();
     }
 }
