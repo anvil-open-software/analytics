584ce3bab462f1450d3b5d7d1ff80d78a3493425
mitchemr <michael.mitchell@dematic.com>
2015-07-02 13:45:29 -0700

    DLABS-316, persists events to dynamo in batches.

--- store/src/main/java/com/dematic/labs/analytics/store/sparks/drivers/Persister.java
+++ store/src/main/java/com/dematic/labs/analytics/store/sparks/drivers/Persister.java
@@ -14,14 +14,21 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 import java.io.Serializable;
 import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
 
-import static com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig.TableNameOverride.*;
+import static com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig.TableNameOverride.withTableNamePrefix;
 import static com.dematic.labs.analytics.common.sparks.DriverUtils.getJavaDStream;
 import static com.dematic.labs.analytics.common.sparks.DriverUtils.getStreamingContext;
 import static com.dematic.labs.toolkit.aws.Connections.createDynamoTable;
+import static java.util.Spliterators.spliteratorUnknownSize;
+import static java.util.stream.StreamSupport.stream;
 
 public final class Persister implements Serializable {
     public static final String RAW_EVENT_LEASE_TABLE_NAME = "Raw_Event_LT";
+
+    //todo: handle checkpoiting
 
     public static void main(final String[] args) {
         if (args.length < 3) {
@@ -55,18 +62,25 @@ public final class Persister implements Serializable {
                 inputStream.map(
                         event -> EventUtils.jsonToEvent(new String(event, Charset.defaultCharset()))
                 );
+        //todo: handle dynamo failures and driver failures...
         // save by partition
-        eventStream.foreachRDD(v1 -> {
-            v1.foreachPartition(eventIterator -> {
-                //todo look into batching the events and time to create dynamo connection and figure out exception handling
+        eventStream.foreachRDD(rdd -> {
+            rdd.foreachPartition(eventIterator -> {
                 final AmazonDynamoDBClient amazonDynamoDBClient = Connections.getAmazonDynamoDBClient(dynamoDBEndpoint);
-                eventIterator.forEachRemaining(event -> {
-                    final DynamoDBMapper dynamoDBMapper = StringUtils.isNullOrEmpty(tablePrefix) ?
-                            new DynamoDBMapper(amazonDynamoDBClient) :
-                            new DynamoDBMapper(amazonDynamoDBClient, new DynamoDBMapperConfig(withTableNamePrefix(tablePrefix)));
-                    dynamoDBMapper.save(event);});
+                final DynamoDBMapper dynamoDBMapper = StringUtils.isNullOrEmpty(tablePrefix) ?
+                        new DynamoDBMapper(amazonDynamoDBClient) :
+                        new DynamoDBMapper(amazonDynamoDBClient, new DynamoDBMapperConfig(withTableNamePrefix(tablePrefix)));
+                // turn the eventIterator to a list of events and batch save
+                final List<DynamoDBMapper.FailedBatch> failedBatches = dynamoDBMapper.batchSave(stream(
+                        spliteratorUnknownSize(eventIterator, Spliterator.CONCURRENT), true).collect(
+                        Collectors.<Event>toList()));
+                //todo: handle batch failures
+                System.out.println(" ---------- " + failedBatches);
+
             });
             return null;
         });
     }
+
+
 }
