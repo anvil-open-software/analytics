d88355a6a629dc9a4a2fb738be9423f5b30e7d8c
mitchemr <michael.mitchell@dematic.com>
2015-10-01 11:56:24 -0700

    DLABS-419, make receiver idempotent, removed cache. -- duplicate w state, fixed tests.

--- ingestion/src/test/java/com/dematic/labs/analytics/ingestion/drivers/EventStreamAggregatorTest.java
+++ ingestion/src/test/java/com/dematic/labs/analytics/ingestion/drivers/EventStreamAggregatorTest.java
@@ -5,8 +5,8 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedScanList;
 import com.dematic.labs.analytics.common.sparks.DriverUtils;
-import com.dematic.labs.analytics.ingestion.sparks.tables.EventAggregator;
 import com.dematic.labs.analytics.ingestion.sparks.drivers.EventStreamAggregator;
+import com.dematic.labs.analytics.ingestion.sparks.tables.EventAggregator;
 import com.dematic.labs.toolkit.SystemPropertyRule;
 import com.dematic.labs.toolkit.aws.KinesisStreamRule;
 import com.jayway.awaitility.Awaitility;
@@ -16,8 +16,10 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
+import org.junit.rules.TemporaryFolder;
 import org.junit.rules.TestRule;
 
+import java.io.IOException;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -35,22 +37,25 @@ import static org.junit.Assert.assertTrue;
 public final class EventStreamAggregatorTest {
     // number of events
     private static final long EVENT_COUNT = 50;
+    // create a tmp dir
+    private TemporaryFolder folder = new TemporaryFolder();
 
     // setup Kinesis
     private final KinesisStreamRule kinesisStreamRule = new KinesisStreamRule();
     // load system properties from file and Rules
     @Rule
     public final TestRule systemPropertyRule =
-            RuleChain.outerRule(new SystemPropertyRule()).around(kinesisStreamRule);
+            RuleChain.outerRule(new SystemPropertyRule()).around(kinesisStreamRule).around(folder);
 
     @Test
-    public void aggregateByMinute() {
+    public void aggregateByMinute() throws IOException {
         // start sparks driver, running in the background
         final String kinesisEndpoint = System.getProperty("kinesisEndpoint");
         final String kinesisInputStream = System.getProperty("kinesisInputStream");
         final String dynamoDBEndpoint = System.getProperty("dynamoDBEndpoint");
         // append user name to ensure tables are unique to person running tests to avoid collisions
         final String userNamePrefix = System.getProperty("user.name") + "_";
+        final String checkpointDir = folder.getRoot().getAbsolutePath();
 
         // create the dynamo event table
         final String tableName = createDynamoTable(System.getProperty("dynamoDBEndpoint"), EventAggregator.class,
@@ -64,7 +69,7 @@ public final class EventStreamAggregatorTest {
         // Must add 1 more thread than the number of receivers or the output won't show properly from the driver
         final int numSparkThreads = DriverUtils.getNumberOfShards(kinesisEndpoint, kinesisInputStream) + 1;
         final JavaStreamingContext streamingContext = getStreamingContext("local[" + numSparkThreads + "]",
-                leaseTable, null, pollTime);
+                leaseTable, checkpointDir, pollTime);
 
         try {
             final ExecutorService executorService = Executors.newCachedThreadPool();
@@ -102,6 +107,8 @@ public final class EventStreamAggregatorTest {
             // delete the lease table, always in the east
             deleteDynamoLeaseManagerTable(getAmazonDynamoDBClient(System.getProperty("dynamoDBEndpoint")),
                     leaseTable);
+            // remove the checkpoint dir
+            folder.delete();
         }
     }
 
