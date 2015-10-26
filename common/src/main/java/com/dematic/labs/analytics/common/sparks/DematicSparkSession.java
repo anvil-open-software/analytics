64b973b6bc9a7f9d74d592956ec1459aebc7e8d7
hejix <heji.kim@gmail.com>
2015-10-26 16:03:48 -0700

    DLABS-413 make Serializable and slim down the uber jar with unnecessary AWS jars

--- common/src/main/java/com/dematic/labs/analytics/common/sparks/DematicSparkSession.java
+++ common/src/main/java/com/dematic/labs/analytics/common/sparks/DematicSparkSession.java
@@ -6,10 +6,13 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Serializable;
+
 /**
- *
+ * Holder for spark driver session details including dstream
  */
-public class DematicSparkSession {
+public class DematicSparkSession implements Serializable {
+
 
     JavaStreamingContext streamingContext;
     JavaDStream<byte[]> dStreams;
@@ -19,6 +22,8 @@ public class DematicSparkSession {
 
     String checkPointDir;
     String appName;
+
+    private static final long serialVersionUID = 1896518324147474596L;
     private static final Logger LOGGER = LoggerFactory.getLogger(DematicSparkSession.class);
 
     public DematicSparkSession(String appName, String awsEndPoint, String streamName) {
@@ -72,4 +77,33 @@ public class DematicSparkSession {
         LOGGER.info("using >{}< checkpoint dir", checkPointDir);
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DematicSparkSession that = (DematicSparkSession) o;
+
+        if (streamingContext != null ? !streamingContext.equals(that.streamingContext) : that.streamingContext != null)
+            return false;
+        if (dStreams != null ? !dStreams.equals(that.dStreams) : that.dStreams != null) return false;
+        if (streamName != null ? !streamName.equals(that.streamName) : that.streamName != null) return false;
+        if (awsEndPoint != null ? !awsEndPoint.equals(that.awsEndPoint) : that.awsEndPoint != null) return false;
+        if (checkPointDir != null ? !checkPointDir.equals(that.checkPointDir) : that.checkPointDir != null)
+            return false;
+        return !(appName != null ? !appName.equals(that.appName) : that.appName != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = streamingContext != null ? streamingContext.hashCode() : 0;
+        result = 31 * result + (dStreams != null ? dStreams.hashCode() : 0);
+        result = 31 * result + (streamName != null ? streamName.hashCode() : 0);
+        result = 31 * result + (awsEndPoint != null ? awsEndPoint.hashCode() : 0);
+        result = 31 * result + (checkPointDir != null ? checkPointDir.hashCode() : 0);
+        result = 31 * result + (appName != null ? appName.hashCode() : 0);
+        return result;
+    }
 }
