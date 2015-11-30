98fdc0d6cddcb02980ed3a4330884b4e6f535f3f
mitchemr <michael.mitchell@dematic.com>
2015-11-30 13:19:54 -0800

    DLABS-498, Updating test.

--- ingestion/src/test/java/com/dematic/labs/analytics/ingestion/drivers/InterArrivalTimeProcessorTest.java
+++ ingestion/src/test/java/com/dematic/labs/analytics/ingestion/drivers/InterArrivalTimeProcessorTest.java
@@ -1,7 +1,8 @@
 package com.dematic.labs.analytics.ingestion.drivers;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.dematic.labs.analytics.common.sparks.DriverConsts;
 import com.dematic.labs.analytics.ingestion.sparks.drivers.InterArrivalTimeProcessor;
-import com.dematic.labs.analytics.ingestion.sparks.tables.EventAggregator;
 import com.dematic.labs.analytics.ingestion.sparks.tables.InterArrivalTimeBucket;
 import com.dematic.labs.toolkit.SystemPropertyRule;
 import com.dematic.labs.toolkit.aws.KinesisStreamRule;
@@ -15,12 +16,12 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static com.dematic.labs.analytics.ingestion.sparks.drivers.InterArrivalTimeProcessor.INTER_ARRIVAL_TIME_LEASE_TABLE_NAME;
-import static com.dematic.labs.toolkit.aws.Connections.createDynamoTable;
+import static com.dematic.labs.toolkit.aws.Connections.deleteDynamoTable;
+import static com.dematic.labs.toolkit.aws.Connections.getAmazonDynamoDBClient;
 
 public final class InterArrivalTimeProcessorTest {
     // create a tmp dir
     private TemporaryFolder folder = new TemporaryFolder();
-
     // setup Kinesis
     private final KinesisStreamRule kinesisStreamRule = new KinesisStreamRule();
     // load system properties from file and Rules
@@ -36,20 +37,30 @@ public final class InterArrivalTimeProcessorTest {
         final String dynamoDBEndpoint = System.getProperty("dynamoDBEndpoint");
         // append user name to ensure tables are unique to person running tests to avoid collisions
         final String userNamePrefix = System.getProperty("user.name") + "_";
+        // set the checkpoint dir
         final String checkpointDir = folder.getRoot().getAbsolutePath();
-
-        // create the dynamo event table
-        final String tableName = createDynamoTable(System.getProperty("dynamoDBEndpoint"), InterArrivalTimeBucket.class,
-                userNamePrefix);
-        final String leaseTable = String.format("%s%s", userNamePrefix, INTER_ARRIVAL_TIME_LEASE_TABLE_NAME);
+        System.setProperty(DriverConsts.SPARK_CHECKPOINT_DIR, checkpointDir);
 
         try {
             final ExecutorService executorService = Executors.newCachedThreadPool();
             executorService.submit(() -> {
-                InterArrivalTimeProcessor.main(new String[]{"", ""});
+                InterArrivalTimeProcessor.main(new String[]{kinesisEndpoint, kinesisInputStream, dynamoDBEndpoint,
+                        userNamePrefix, "3"});
             });
 
         } finally {
+            // delete dynamo tables
+            final AmazonDynamoDBClient amazonDynamoDBClient = getAmazonDynamoDBClient(dynamoDBEndpoint);
+            try {
+                final String tableName = String.format("%s_%s", userNamePrefix, InterArrivalTimeBucket.TABLE_NAME);
+                deleteDynamoTable(amazonDynamoDBClient, tableName);
+            } catch (final Throwable ignore) {
+            }
+            try {
+                final String leaseTable = String.format("%s%s", userNamePrefix, INTER_ARRIVAL_TIME_LEASE_TABLE_NAME);
+                deleteDynamoTable(amazonDynamoDBClient, leaseTable);
+            } catch (final Throwable ignore) {
+            }
 
         }
     }
