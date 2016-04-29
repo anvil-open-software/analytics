b2672dcaafe6b5ad18469893222d507bf277b10b
mitchemr <michael.mitchell@dematic.com>
2016-04-29 14:26:18 -0700

    DLABS-691, compute signal aggregations.

---
+++ ingestion/src/main/java/com/dematic/labs/analytics/ingestion/spark/drivers/grainger/AggregationFunctions.java
@@ -0,0 +1,39 @@
+package com.dematic.labs.analytics.ingestion.spark.drivers.grainger;
+
+import com.dematic.labs.toolkit.communication.Signal;
+import org.apache.spark.sql.expressions.Aggregator;
+
+import java.io.Serializable;
+
+final class AggregationFunctions implements Serializable {
+    private AggregationFunctions() {
+    }
+
+    static class ComputeSignalAggregation extends Aggregator<Signal, Long, SignalAggregation> {
+        private final SignalAggregation signalAggregation;
+
+        public ComputeSignalAggregation() {
+            signalAggregation = new SignalAggregation();
+        }
+
+        @Override
+        public Long zero() {
+            return 0L;
+        }
+
+        @Override
+        public Long reduce(final Long value, final Signal signal) {
+            return value + signal.getValue();
+        }
+
+        @Override
+        public SignalAggregation finish(final Long reduction) {
+            return signalAggregation;
+        }
+
+        @Override
+        public Long merge(final Long valueOne, final Long valueTwo) {
+            return valueOne + valueTwo;
+        }
+    }
+}
---
+++ ingestion/src/main/java/com/dematic/labs/analytics/ingestion/spark/drivers/grainger/SignalAggregation.java
@@ -0,0 +1,7 @@
+package com.dematic.labs.analytics.ingestion.spark.drivers.grainger;
+
+//{"OPCTagID":1549,"OPCMetricID":25,"Sum":7.875713721263E12,"Count":7316,"Average":1.0765054293689175E9,"Minimum":"1000954613","Maximum":"999917866"}
+
+class SignalAggregation {
+
+}
