6287ff15208f0a3ad81299bc35b0a51ee3e155b0
mitchemr <michael.mitchell@dematic.com>
2016-05-03 06:33:15 -0700

    DLABS-691, added opcTagId

--- ingestion/src/main/java/com/dematic/labs/analytics/ingestion/spark/drivers/grainger/SignalAggregation.java
+++ ingestion/src/main/java/com/dematic/labs/analytics/ingestion/spark/drivers/grainger/SignalAggregation.java
@@ -8,17 +8,17 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
 
-public class SignalAggregation implements Serializable {
+class SignalAggregation implements Serializable {
+    private final String opcTagId;
     private Long count;
     private Long sum;
     private Long min;
     private Long max;
 
-    public SignalAggregation() {
+    public SignalAggregation(final String opcTagId) {
+        this.opcTagId = opcTagId;
         count = 0L;
         sum = 0L;
-        min = 0L;
-        max = 0L;
     }
 
     SignalAggregation computeAggregations(final List<Signal> values) {
@@ -38,44 +38,36 @@ public class SignalAggregation implements Serializable {
     }
 
     private void minMax(final Long value) {
-        min = min <= value ? min : value;
-        max = max >= value ? max : value;
+        if (min == null || min > value) {
+            min = value;
+        }
+        if (max == null || max < value) {
+            max = value;
+        }
     }
 
     private void sum(final Long value) {
         sum = sum + value;
     }
 
-    public Long getCount() {
-        return count;
+    public String getOpcTagId() {
+        return opcTagId;
     }
 
-    public void setCount(Long count) {
-        this.count = count;
+    public Long getCount() {
+        return count;
     }
 
     public Long getSum() {
         return sum;
     }
 
-    public void setSum(Long sum) {
-        this.sum = sum;
-    }
-
     public Long getMin() {
         return min;
     }
 
-    public void setMin(Long min) {
-        this.min = min;
-    }
-
     public Long getMax() {
         return max;
-    }
-
-    public void setMax(Long max) {
-        this.max = max;
     }
 
     public Double getAvg() {
@@ -87,7 +79,8 @@ public class SignalAggregation implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SignalAggregation that = (SignalAggregation) o;
-        return Objects.equals(count, that.count) &&
+        return Objects.equals(opcTagId, that.opcTagId) &&
+                Objects.equals(count, that.count) &&
                 Objects.equals(sum, that.sum) &&
                 Objects.equals(min, that.min) &&
                 Objects.equals(max, that.max);
@@ -95,13 +88,14 @@ public class SignalAggregation implements Serializable {
 
     @Override
     public int hashCode() {
-        return Objects.hash(count, sum, min, max);
+        return Objects.hash(opcTagId, count, sum, min, max);
     }
 
     @Override
     public String toString() {
         return "SignalAggregation{" +
-                "count=" + count +
+                "opcTagId='" + opcTagId + '\'' +
+                ", count=" + count +
                 ", sum=" + sum +
                 ", min=" + min +
                 ", max=" + max +
