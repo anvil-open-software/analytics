afaacf5e45d3dc3175f519175c6e7c3c4b0e5556
mitchemr <michael.mitchell@dematic.com>
2016-05-11 14:20:08 -0700

    DLABS-665, create kafka streaming context.

--- store/src/main/java/com/dematic/labs/analytics/store/spark/drivers/signal/kafka/CassandraPersister.java
+++ store/src/main/java/com/dematic/labs/analytics/store/spark/drivers/signal/kafka/CassandraPersister.java
@@ -1,4 +1,8 @@
 package com.dematic.labs.analytics.store.spark.drivers.signal.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class CassandraPersister {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CassandraPersister.class);
 }
