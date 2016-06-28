102aa76d481dc799b5e39b2dca01373d2e22c2e6
mitchemr <michael.mitchell@dematic.com>
2016-06-27 21:44:19 -0700

    DLABS-732, added test to test signal aggregation.

--- ingestion/src/test/java/com/dematic/labs/analytics/ingestion/spark/drivers/signal/cassandra/SignalAndSignalAggregation.java
+++ ingestion/src/test/java/com/dematic/labs/analytics/ingestion/spark/drivers/signal/cassandra/SignalAndSignalAggregation.java
@@ -2,6 +2,7 @@ package com.dematic.labs.analytics.ingestion.spark.drivers.signal.cassandra;
 
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
+import com.dematic.labs.analytics.ingestion.spark.drivers.signal.SignalAggregation;
 import com.dematic.labs.toolkit.SystemPropertyRule;
 import com.dematic.labs.toolkit.cassandra.EmbeddedCassandraRule;
 import com.dematic.labs.toolkit.communication.Signal;
@@ -16,11 +17,9 @@ import java.time.ZoneId;
 import java.util.stream.IntStream;
 
 public final class SignalAndSignalAggregation {
-
     private EmbeddedCassandraRule cassandraUnit = new EmbeddedCassandraRule();
     @Rule
-    public final TestRule systemPropertyRule =
-            RuleChain.outerRule(new SystemPropertyRule()).around(cassandraUnit);
+    public final TestRule systemPropertyRule = RuleChain.outerRule(new SystemPropertyRule()).around(cassandraUnit);
 
     @Test
     public void querySignals() {
@@ -45,7 +44,7 @@ public final class SignalAndSignalAggregation {
     private void insertSignals(final Session session, final String keyspace, final int numOfSignals) {
         final int[] ints = {1};
         IntStream.range(0, numOfSignals).forEach(signal -> {
-            final ResultSet z = session.execute("INSERT INTO " + keyspace + "." + Signal.TABLE_NAME +
+            session.execute("INSERT INTO " + keyspace + "." + Signal.TABLE_NAME +
                     " (unique_id, id, value, day, timestamp, quality, opc_tag_reading_id, opc_tag_id, proxied_type_name, extended_properties) " +
                     "VALUES (" +
                     "'null'," +
@@ -59,14 +58,45 @@ public final class SignalAndSignalAggregation {
                     "'Odatech.Business.Integration.OPCTagReading'," +
                     "['test']);"
             );
-            System.out.println(z.all());
             ints[0] = ints[0] + 1;
         });
     }
 
     @Test
     public void querySignalAggregation() {
-        // ensure all keyspaces are clean
+        // 1) ensure all keyspaces are clean
         cassandraUnit.dropKeySpace();
+        // 2) create the keyspace name
+        final String keyspace = System.getProperty("user.name") + "_keyspace";
+        try (final Session session = cassandraUnit.getCluster().connect()) {
+            // 3) create the keyspace
+            session.execute("CREATE KEYSPACE " + keyspace + " WITH replication = {'class':'SimpleStrategy', " +
+                    "'replication_factor':3};");
+            // 4) create the table
+            session.execute(SignalAggregation.createTableCql(keyspace));
+            // 5) add signal aggregations
+            insertSignalAggregation(session, keyspace, 50);
+            // 6) query signal aggregation table
+            final ResultSet execute = session.execute("SELECT * FROM " + keyspace + "." +
+                    SignalAggregation.TABLE_NAME + ";");
+            Assert.assertEquals(50, execute.all().size());
+        }
+    }
+
+    private void insertSignalAggregation(final Session session, final String keyspace, final int numOfSignals) {
+        final int[] ints = {1};
+        IntStream.range(0, numOfSignals).forEach(signal -> {
+            session.execute("INSERT INTO " + keyspace + "." + SignalAggregation.TABLE_NAME +
+                    " (opc_tag_id, aggregate, count, sum, min, max) " +
+                    "VALUES (" +
+                    ints[0] + "," +
+                    "'2016-06-17T17:16:00Z'," +
+                    ints[0] + "," +
+                    ints[0] + "," +
+                    ints[0] + "," +
+                    ints[0] + ");"
+            );
+            ints[0] = ints[0] + 1;
+        });
     }
 }
