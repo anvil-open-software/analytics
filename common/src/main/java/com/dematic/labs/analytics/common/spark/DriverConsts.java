8f98f286bff1cabc270e2593f2fffef8e31c15ab
mitchemr <michael.mitchell@dematic.com>
2017-05-01 13:57:20 -0700

    DLABS-1085, added a driver to save signals to parquet.

--- common/src/main/java/com/dematic/labs/analytics/common/spark/DriverConsts.java
+++ common/src/main/java/com/dematic/labs/analytics/common/spark/DriverConsts.java
@@ -10,6 +10,7 @@ public interface DriverConsts {
     String SPARK_QUERY_TRIGGER = "spark.query.trigger";
     String SPARK_CHECKPOINT_DIR = "spark.checkpoint.dir";
     String SPARK_STREAMING_CHECKPOINT_DIR = "spark.sql.streaming.checkpointLocation";
+    String SPARK_PARQUET_DIR = "spark.parquet.dir";
     String SPARK_DRIVER_VALIDATE_COUNTS = "dematiclabs.driver.validate.counts";
     String SPARK_QUERY_STATISTICS = "dematiclabs.driver.query.statistics";
 }
---
+++ diagnostics/src/main/scala/com/dematic/labs/analytics/diagnostics/spark/drivers/StructuredStreamingParquetPersister.scala
@@ -0,0 +1,69 @@
+package com.dematic.labs.analytics.diagnostics.spark.drivers
+
+import com.dematic.labs.analytics.common.spark.DriverConsts._
+import com.dematic.labs.analytics.common.spark.KafkaStreamConfig.{KAFKA_ADDITIONAL_CONFIG_PREFIX, getPrefixedSystemProperties}
+import com.dematic.labs.analytics.diagnostics.spark.drivers.PropertiesUtils.getOrThrow
+import org.apache.parquet.Strings
+import org.apache.spark.sql.{Encoders, SparkSession}
+
+/**
+  * Driver will save json signals to parquet data storage.
+  *
+  **/
+object StructuredStreamingParquetPersister {
+  private val APP_NAME = "SS_Parquet_Persister"
+
+  def main(args: Array[String]) {
+    if (args.length < 2) {
+      System.err.println("Usage: StructuredStreamingParquetPersister <brokerBootstrapServers> <topic> <optional masterUrl>")
+      System.exit(1)
+    }
+
+    // set parameters
+    val brokers = args(0)
+    val topics = args(1)
+    val masterUrl = if (args.length == 3) args(2) else null
+
+    // spark system properties
+    val sqlPartitions = sys.props(SPARK_SQL_SHUFFLE_PARTITIONS)
+    val checkpointDir = getOrThrow(SPARK_CHECKPOINT_DIR)
+    val parquetDir = getOrThrow(SPARK_PARQUET_DIR)
+    // kafka options
+    val kafkaOptions = getPrefixedSystemProperties(KAFKA_ADDITIONAL_CONFIG_PREFIX)
+
+    // create the spark session
+    val builder: SparkSession.Builder = SparkSession.builder
+    if (!Strings.isNullOrEmpty(masterUrl)) {
+      builder.master(masterUrl)
+    }
+    builder.appName(APP_NAME)
+
+    builder.config(SPARK_STREAMING_CHECKPOINT_DIR, checkpointDir)
+    val spark: SparkSession = builder.getOrCreate
+    // set sql partitions if set
+    if (!Strings.isNullOrEmpty(sqlPartitions)) spark.sql("SET spark.sql.shuffle.partitions=" + sqlPartitions)
+
+    // spark.sparkContext.setLogLevel("DEBUG")
+
+    // read from the kafka steam
+    val kafka = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", brokers)
+      .option("subscribe", topics)
+      .option("startingOffsets", "earliest")
+      .options(kafkaOptions)
+      .load
+
+    // kafka schema is the following: input columns: [value, timestamp, timestampType, partition, key, topic, offset]
+    val signals = kafka.selectExpr("CAST(value AS STRING)").as(Encoders.STRING)
+
+    // write aggregate sinks to parquet
+    val out = signals.writeStream
+      .queryName("parquet_persister")
+      .format("parquet")
+      .option("path", parquetDir)
+      .option("checkpointLocation", checkpointDir)
+      .start
+    out.awaitTermination()
+  }
+}
