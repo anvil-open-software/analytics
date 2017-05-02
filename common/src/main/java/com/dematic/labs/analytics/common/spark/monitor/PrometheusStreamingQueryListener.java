c45a0a2989ff43a3bc1747a98c6747a1c3e305e5
kimh <heji.kim.ext@dematic.com>
2017-05-02 11:35:52 -0700

    DLABS-1183 add label for driver

--- common/src/main/java/com/dematic/labs/analytics/common/spark/monitor/PrometheusStreamingQueryListener.java
+++ common/src/main/java/com/dematic/labs/analytics/common/spark/monitor/PrometheusStreamingQueryListener.java
@@ -10,50 +10,52 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.URLEncoder;
 
 /**
- *
  * Uses prometheus java client https://github.com/prometheus/client_java
- *  to register structured streaming query stats
+ * to register structured streaming query stats to a push gateway.
  */
 
 public class PrometheusStreamingQueryListener extends StreamingQueryListener {
-    private static String job_name="spark-push-gateway";
-    private static String SPARK_METRIC_PREFIX="spark_structured_streaming_";
+
+    public static final String SPARK_METRIC_PREFIX = "spark_structured_streaming_";
+    public static final String LABEL_NAME = "driver";
+    // might have to parameterize this
+    public static final String job_name = "spark-push-gateway";
     private String push_gateway_host;
 
 
     private String app_name;
     CollectorRegistry collectorRegistry;
 
- // collectors for query
+    // collectors for spark streaming interactive query stats
     private Counter total_batches;
     private Counter total_input_rows;
     private Gauge processedRowsPerSecond;
     private Gauge inputRowsPerSecond;
-    private boolean addSparkQueryStats=true;
+
+    private boolean addSparkQueryStats = true;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PrometheusStreamingQueryListener.class);
 
-    public PrometheusStreamingQueryListener(String spark_app_name, String push_gateway_host){
+    public PrometheusStreamingQueryListener(String spark_app_name, String push_gateway_host) {
         LOGGER.info("Push gateway statistics posted to: " + push_gateway_host);
         this.push_gateway_host = push_gateway_host;
-        app_name=spark_app_name;
-        collectorRegistry=CollectorRegistry.defaultRegistry;
+        app_name = spark_app_name;
+        collectorRegistry = CollectorRegistry.defaultRegistry;
         collectorRegistry.clear();
 
         // add all standard jvm collectors - memory, gc, machine, etc..
         DefaultExports.initialize();
 
-        total_batches= Counter.build().name(SPARK_METRIC_PREFIX+"batches_total")
-                .labelNames(app_name).help("Total number of batches.").register();
-        total_input_rows= Counter.build().name(SPARK_METRIC_PREFIX+"input_rows_total")
-                .labelNames(app_name).help("Total number of input events.").register();
-        inputRowsPerSecond= Gauge.build().name(SPARK_METRIC_PREFIX+"input_rows_per_second")
-                .labelNames(app_name).help("Total number of input events.").register();
-        processedRowsPerSecond= Gauge.build().name(SPARK_METRIC_PREFIX+"processed_rows_per_second")
-                .labelNames(app_name).help("Total number of input events.").register();
+        total_batches = Counter.build().labelNames(LABEL_NAME).name(SPARK_METRIC_PREFIX + "batches_total")
+                .labelNames(LABEL_NAME).help("Total number of batches.").register();
+        total_input_rows = Counter.build().name(SPARK_METRIC_PREFIX + "input_rows_total")
+                .labelNames(LABEL_NAME).help("Total number of input events.").register();
+        inputRowsPerSecond = Gauge.build().name(SPARK_METRIC_PREFIX + "input_rows_per_second")
+                .labelNames(LABEL_NAME).help("Total number of input events.").register();
+        processedRowsPerSecond = Gauge.build().name(SPARK_METRIC_PREFIX + "processed_rows_per_second")
+                .labelNames(LABEL_NAME).help("Total number of input events.").register();
 
     }
 
@@ -65,11 +67,13 @@ public class PrometheusStreamingQueryListener extends StreamingQueryListener {
     public void setAddSparkQueryStats(boolean addSparkQueryStats) {
         this.addSparkQueryStats = addSparkQueryStats;
     }
-   public String getMetricsURLBase() {
-       String base_url = "http://" + push_gateway_host + "/metrics/job/";
-       //URLEncoder.encode(job_name, "UTF-8");
-       return base_url;
-   }
+
+    public String getMetricsURLBase() {
+        String base_url = "http://" + push_gateway_host + "/metrics/job/";
+        //URLEncoder.encode(job_name, "UTF-8");
+        return base_url;
+    }
+
     @Override
     public void onQueryProgress(QueryProgressEvent event) {
         PushGateway pg = new PushGateway(push_gateway_host);
@@ -83,8 +87,8 @@ public class PrometheusStreamingQueryListener extends StreamingQueryListener {
 
             pg.pushAdd(collectorRegistry, job_name);
         } catch (IOException e) {
-                 LOGGER.error("Error in query progress statistic written to " + getMetricsURLBase() +
-                         "with error  \n" + e.getMessage());
+            LOGGER.error("Error in query progress statistic writing to " + getMetricsURLBase() +
+                    " with error  \n" + e.getMessage());
         }
 
     }
